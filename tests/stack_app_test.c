71f8f6272fe8fd1e83cac1c382d92d2ab3d64113
Rahul Chaudhary <rahulchaudharyofficial@outlook.com>
2021-05-08 17:06:40 +0530

    more unit test related changes

--- tests/stack_app_test.c
+++ tests/stack_app_test.c
@@ -39,3 +39,42 @@ Test(stack_app_test, reverse_number_app) {
     output[i] = '\0';
     cr_assert(strcmp(output,expected_output)==0,"Must be equal");
 }
+
+bool array_eq(const int* a1, const int* a2) {
+    
+    if(sizeof(a1) == sizeof(a2)) {
+        bool eq = true;
+        for(int i=0; i< sizeof(a1);i++) {
+            if(*(a1+i) != *(a2+i)) {
+                eq = false;
+                break;
+            }
+        }
+        return eq;
+    }
+    else {
+        return false;
+    }
+}
+
+Test(stack_app_test, decimal_to_binary) {
+    int num = 100;
+    int* digit;
+    int output[7];
+    int expected_output[] = {1,1,0,0,1,0,0};
+    while(num>0) {
+        digit = (int*) malloc(sizeof(int));
+        *digit = num % 2;
+        push (stack, digit);
+        num/=2;
+    }
+
+    int i=0;
+    while(!empty_stack(stack)) {
+        int* c = (int*) pop(stack);
+        output[i] = *c;
+        i++;
+    }
+    printf("\n");
+    cr_assert(array_eq(output,expected_output)==0,"Must be equal");
+}
