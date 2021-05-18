c9459f7db6f965ab2553ba5d4ef50bf44605d643
rahul chaudhary <rahul.chaudhary.1711@gmail.com>
2021-05-18 19:21:33 +0530

    More changes coming through

--- src/postfix_eval.c
+++ src/postfix_eval.c
@@ -9,7 +9,6 @@
 int postfix_evaluation(char* postfix_expression)
 {
     //sanity Check
-    assert(stack != NULL);
     assert(postfix_expression != NULL);
 
     //If all well then start evaluation process
@@ -42,17 +41,3 @@ int postfix_evaluation(char* postfix_expression)
     destroy(stack);
     return result;
 }
-
-/*
-int main(int argc, char const *argv[])
-{
-    stack_t* stack = create_stack();
-    int result1 = postfix_evaluation(stack, "12+3*");
-    assert(result1 == 9);
-
-    int result2 = postfix_evaluation(stack, "12+34+*");
-    assert(result2 == 21);
-    destroy(stack);
-    return 0;
-}
-*/
