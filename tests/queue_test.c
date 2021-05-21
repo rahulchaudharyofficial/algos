38fd1034cc0287af464993472c6da97d85bbc4f3
rahul chaudhary <rahul.chaudhary.1711@gmail.com>
2021-05-21 11:30:22 +0530

    more testing on queues

--- tests/queue_test.c
+++ tests/queue_test.c
@@ -23,24 +23,6 @@ Test(queue_operations_test, create_queue_test)
 	cr_assert(queue != NULL, "Queue must not be NULL");
 }
 
-void print_queue()
-{
-	/*
-	int* data = NULL;
-	if(queue_size(queue)>0)
-	{
-		data = (int*) dequeue(queue);
-		printf("data = %d\n", *data);
-		while(data != NULL)
-		{
-			printf("data = %d\n", *data);
-			data = (int*) dequeue(queue);
-		}
-		printf("\n");
-	}
-	*/
-}
-
 Test(queue_operations_test, enqueue_test)
 {
 	int* dataPtr = NULL;
@@ -57,4 +39,63 @@ Test(queue_operations_test, enqueue_test)
 		int* data = (int*) dequeue(queue);
 		cr_assert(*data == i, "Must be equal");
 	}	
+}
+
+Test(queue_operations_test, dequeue_test)
+{
+	int* dataPtr = NULL;
+	for(int i = 1; i <= 10; i++)
+	{
+		dataPtr = (int*) malloc(sizeof(i));
+		*dataPtr = i;
+		enqueue(queue, dataPtr);
+	}
+
+	cr_assert(queue_size(queue) == 10, "Must be equals");
+	for(int i = 1; i <= 10; i++)
+	{
+		int* data = (int*) dequeue(queue);
+		cr_assert(*data == i, "Must be equal");
+	}
+	cr_assert(queue_size(queue) == 0, "Queue must be empty");
+}
+
+Test(queue_operations_test, front_test)
+{
+	int* dataPtr = NULL;
+	for(int i = 1; i <= 5; i++)
+	{
+		dataPtr = (int*) malloc(sizeof(i));
+		*dataPtr = i;
+		enqueue(queue, dataPtr);
+	}
+
+	dataPtr = (int*) front(queue);	
+	cr_assert(*dataPtr == 1, "Queue front must be 1");
+}
+
+Test(queue_operations_test, rear_test)
+{
+	int* dataPtr = NULL;
+	for(int i = 1; i <= 5; i++)
+	{
+		dataPtr = (int*) malloc(sizeof(i));
+		*dataPtr = i;
+		enqueue(queue, dataPtr);
+	}
+
+	dataPtr = (int*) rear(queue);	
+	cr_assert(*dataPtr == 5, "Queue rear must be 5");
+}
+
+Test(queue_operations_test, queue_size_test)
+{
+	int* dataPtr = NULL;
+	for(int i = 1; i <= 5; i++)
+	{
+		dataPtr = (int*) malloc(sizeof(i));
+		*dataPtr = i;
+		enqueue(queue, dataPtr);
+	}
+	cr_assert(queue_size(queue) == 5, "Queue size must be 5");
 }
