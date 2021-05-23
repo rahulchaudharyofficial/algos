bdf338b2862d27552950570f87a0edba4d83778c
rahul chaudhary <rahul.chaudhary.1711@gmail.com>
2021-05-23 17:53:33 +0530

    rahul

---
+++ src/bst.c
@@ -0,0 +1,18 @@
+#include "bst.h"
+
+struct TreeNode
+{
+    void* value;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+tnode_t* create_tree_node(void* node)
+{
+    
+}
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
---
+++ src/bst.h
@@ -0,0 +1,6 @@
+#ifndef _BST_H_
+#define _BST_H_
+    #include<stdbool.h>
+    typedef struct TreeNode tnode_t;
+    tnode_t* create_tree_node(void*);
+#endif
